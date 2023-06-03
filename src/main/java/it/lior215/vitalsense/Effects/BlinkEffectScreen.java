9f19e1b8925abe8e1fbae01f70758da758bf9436
lior215 <liors492@gmail.com>
2023-06-03 15:24:54 +0200

    added smoother transition

--- src/main/java/it/lior215/vitalsense/Effects/BlinkEffectScreen.java
+++ src/main/java/it/lior215/vitalsense/Effects/BlinkEffectScreen.java
@@ -2,14 +2,21 @@ package it.lior215.vitalsense.Effects;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.*;
+import com.mojang.math.Matrix4f;
 import it.lior215.vitalsense.Event.ModBlinkingEvents;
 import it.lior215.vitalsense.vitalsense;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.RenderBuffers;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderGuiEvent;
+import net.minecraftforge.client.event.RenderGuiOverlayEvent;
+import net.minecraftforge.client.event.RenderHighlightEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -22,16 +29,22 @@ public class BlinkEffectScreen {
     private static final ResourceLocation IMG_LOCATION = new ResourceLocation(vitalsense.MOD_ID, "misc/blink.png");
     protected static int screenWidth;
     protected static int screenHeight;
+    private static int multiplier = 1;
+    static int Timer = 0;
+    static int screenDivider = 11;
 
 
-    public static void Render(double screenDivider) {
+    public static void Render(double screenDivider,int multiplier) {
         Player player = Minecraft.getInstance().player;
         screenWidth = mc.getWindow().getGuiScaledWidth();
         screenHeight = mc.getWindow().getGuiScaledHeight();
+        PoseStack pose = RenderSystem.getModelViewStack();
         assert player != null;
         if (ModBlinkingEvents.getPlayerBlinking()) {
-
-            RenderSystem.disableDepthTest();
+            pose.pushPose();
+            RenderSystem.enableDepthTest();
+            RenderSystem.depthFunc(519);
+            //pose.translate(0.0, 50.0, 0);
             RenderSystem.depthMask(false);
             RenderSystem.enableBlend();
             RenderSystem.setShader(GameRenderer::getPositionTexShader);
@@ -40,24 +53,23 @@ public class BlinkEffectScreen {
             Tesselator tesselator = Tesselator.getInstance();
             BufferBuilder bufferbuilder = tesselator.getBuilder();
             bufferbuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
-            bufferbuilder.vertex(0.0D, (double) screenHeight * (1.0 / screenDivider), -90.0D).uv(0.0F, 1.0F).endVertex(); // IMPORTANT
-            bufferbuilder.vertex(screenWidth, (double) screenHeight * (1.0 / screenDivider), -90.0D).uv(1.0F, 1.0F).endVertex(); // IMPORTANT
+            bufferbuilder.vertex(0.0D, (double) screenHeight/screenDivider * multiplier, -90.0D).uv(0.0F, 1.0F).endVertex(); // IMPORTANT
+            bufferbuilder.vertex(screenWidth, (double) screenHeight/screenDivider * multiplier, -90.0D).uv(1.0F, 1.0F).endVertex(); // IMPORTANT
             bufferbuilder.vertex(screenWidth, 0.0D, -90.0D).uv(1.0F, 0.0F).endVertex(); // IMPORTANT
             bufferbuilder.vertex(0.0D, 0.0D, -90.0D).uv(0.0F, 0.0F).endVertex(); // IMPORTANT
             tesselator.end();
             RenderSystem.depthMask(true);
             RenderSystem.enableDepthTest();
-            RenderSystem.disableBlend();
             RenderSystem.setShaderColor(0.0F, 0.0F, 1.0F, 1.0F);
+            RenderSystem.disableBlend();
+            pose.popPose();
+            RenderSystem.depthFunc(515);
 
         }
     }
 
     @Mod.EventBusSubscriber(modid = vitalsense.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
     public static class RenderBlinkEventClass {
-
-    static int Timer = 0;
-    static int screenDivider = 10;
 
     
     @SubscribeEvent
@@ -66,29 +78,33 @@ public class BlinkEffectScreen {
         if(event.side.isClient() && event.phase == TickEvent.Phase.END && ModBlinkingEvents.getPlayerBlinking()) {
 
             if (Timer > 11 && Timer <= 22) {
-                Render(screenDivider);
-                screenDivider--;
+                Render(screenDivider, multiplier);
+                multiplier++;
                 Timer--;
 
             } else if (Timer > 0 && Timer <= 11 ) {
 
-                screenDivider++;
-                Render(screenDivider);
+                multiplier--;
+                Render(screenDivider, multiplier);
                 Timer--;
             } else if (Timer <= 0) {
+
                 screenDivider = 11;
+                multiplier = 1;
                 Timer = 23;
-                System.out.println("Timer: " + Timer);
                 ModBlinkingEvents.setPlayerBlinking(false);
                 ModBlinkingEvents.setCanStartBlinkingTimer(true);
             } else {
                 Timer -= 1;
-                System.out.println("Timer: " + Timer);
             }
 
         }
-    }
-}
 
+    }
+        @SubscribeEvent
+        public static void playerBlink(RenderGuiOverlayEvent.Post event) {
+        }
+
+    }
 }
 
