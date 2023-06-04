1ebd54ea8b8a82c91300d5666012fdfb8afe16f2
benpospo@gmail.com <93595333+taraxsmallboy@users.noreply.github.com>
2023-06-04 15:38:56 +0200

    Partially working canSeeSky checking

--- src/main/java/it/lior215/vitalsense/Event/ModBreathingEvents.java
+++ src/main/java/it/lior215/vitalsense/Event/ModBreathingEvents.java
@@ -1,28 +1,46 @@
 package it.lior215.vitalsense.Event;
 
 import it.lior215.vitalsense.vitalsense;
-import net.minecraft.client.Minecraft;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = vitalsense.MOD_ID)
+@Mod.EventBusSubscriber(modid = vitalsense.MOD_ID, value = Dist.CLIENT)
 public class ModBreathingEvents {
 
     @SubscribeEvent
-    public static void onClientTick(TickEvent.ClientTickEvent event) {
-        Player player = Minecraft.getInstance().player;
+    public static void onClientTick(TickEvent.PlayerTickEvent event) {
+        Player player = event.player;
         if (event.phase == TickEvent.Phase.END) {
             if (player != null) {
-                if (player.getY() < 100) {
-                    /*String playerY = String.valueOf(player.getY());
-                    player.sendSystemMessage(Component.literal(playerY));*/
+                Level level = player.level;
+                if (player.getY() < 100 && canSeeSky(player, level)) {
+                    player.sendSystemMessage(Component.literal(String.valueOf(level.getHeight())));
                 }
             }
         }
+    }
+
+    private static boolean canSeeSky(Player player, Level world) {
+        BlockPos playerPos = player.getOnPos();
+
+        if (playerPos.getY() >= world.getHeight()) {
+            // Player is above the world height limit, consider it as seeing the sky
+            return true;
+        }
+
+        // Check if there are any non-air blocks between the player's eye position and the sky
+        for (BlockPos blockPos : BlockPos.betweenClosed(playerPos, playerPos.offset(0, world.getHeight(), 0))) {
+            if (!world.getBlockState(blockPos).isAir()) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
