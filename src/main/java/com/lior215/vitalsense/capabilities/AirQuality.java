f52e201cb73662ecd1e0048f0a37db65e780b14b
benpospo@gmail.com <93595333+taraxsmallboy@users.noreply.github.com>
2023-06-14 20:52:18 +0200

    nbt data

--- src/main/java/com/lior215/vitalsense/capabilities/AirQuality.java
+++ src/main/java/com/lior215/vitalsense/capabilities/AirQuality.java
@@ -1,14 +1,19 @@
 package com.lior215.vitalsense.capabilities;
 
+import com.lior215.vitalsense.vitalsense;
 import net.minecraft.client.Minecraft;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraftforge.common.capabilities.AutoRegisterCapability;
 
 import static com.lior215.vitalsense.client.AirQualityOverlay.indicatorX;
+import static com.lior215.vitalsense.vitalsense.LOGGER;
 
 @AutoRegisterCapability
 public class AirQuality {
     int defaultIndicatorPos = Minecraft.getInstance().screen.width / 2 - 94;
     int defaultMaxQuality = 180;
+
+    int airQuality = indicatorX;
 
     public void setAirQuality(int value) {
         indicatorX = value + this.defaultIndicatorPos;
@@ -21,4 +26,15 @@ public class AirQuality {
     public int getMaxAirQuality() {
         return this.defaultMaxQuality;
     }
+
+    public void saveNBTData(CompoundTag nbt) {
+        nbt.putInt("air_quality", airQuality);
+    }
+
+
+    public void loadNBTData(CompoundTag nbt) {
+        LOGGER.info("loaded nbt data for air quality");
+        this.airQuality = nbt.getInt("air_quality");
+    }
+
 }
